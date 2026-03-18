83fa4aee4d1a4a7eb135afb2b4a1b15d80d1097d
Gabrielly Pontes <developergabrielly@gmail.com>
2026-03-18 11:03:43 -0300

    test: add GET /livros/:id test (RED)

--- tests/livros.test.js
+++ tests/livros.test.js
@@ -1,11 +1,20 @@
 const request = require('supertest');
-const app = require('../src/app');
+const api = `http://localhost:${process.env.PORT || 3000}` ;
 
-test('POST /livros criar um livro', async () => {
-    const res = await request(app)
-        .post('/livros')
-        .send({ titulo: 'Clean Code', autor: 'Martin Code' });
-    expect(res.status).toBe(201);
-    expect(res.body.titulo).toBe('Clean Code');
+// test('POST /livros criar um livro', async () => {
+//     const res = await request(api)
+//         .post('/livros')
+//         .send({ titulo: 'Clean Code', autor: 'Martin Code' });
+//     expect(res.status).toBe(201);
+//     expect(res.body.titulo).toBe('Clean Code');
+// });
+
+
+test('GET /livros/:id retorna livro', async () => {
+    const res = await request(api).get('/livros/1');
+
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty('id');
+    expect(res.body).toHaveProperty('titulo');
 });
 
